12eed882d3c9dd1684c82f8283e83c7492ff88a2
Onkar <onkarcharkupalli@gmail.com>
2021-03-10 16:47:06 +0530

    10/3

---
+++ HACKERRANK/CalculatetheNthTerm.c
@@ -0,0 +1,26 @@
+
+//1 2 3 6 11 
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+int find_nth_term(int n, int a, int b, int c) {
+  //Write your code here.
+    int sum = x + y+ z;
+    if(n>4)
+        sum = find_nth_term(n-1, b,c, a+b+c );
+    return sum;
+}
+
+int main() {
+    int n, a, b, c;
+  
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+ 
+    printf("%d", ans); 
+    return 0;
+}
